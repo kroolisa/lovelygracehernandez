167cc9fb193bb4939e7e00f068356ea2b04fa8b1
kroolisa <144514439+kroolisa@users.noreply.github.com>
2024-10-07 22:18:02 +0800

    chore: this is my temporary resume website. temp subdomain under github pages

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.querySelectorAll('.nav-header a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        const offset = 80;
+        const targetPosition = targetSection.getBoundingClientRect().top + window.pageYOffset - offset;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeInSections = document.querySelectorAll('.fade-in');
+
+    const options = {
+        threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    fadeInSections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
